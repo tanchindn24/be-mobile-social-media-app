11bcf29e5fdd8f64fa9501fb52ca04db25923394
tanchindn24 <phamtruong24042001@gmail.com>
2023-12-29 00:36:23 +0700

    post api

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,5 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Post = require("../models/postsModel");
+const User = require("../models/userModel");
 
 const CreatePostController = asyncHandler(async (req, res) => {
   const { idUser, post } = req.body.parameters;
@@ -33,14 +34,52 @@ const CreatePostController = asyncHandler(async (req, res) => {
 });
 
 const GetPostController = asyncHandler(async (req, res) => {
-  const { idUser } = req.body;
-  if (!idUser) {
+  try {
+    const posts = await Post.find().sort({ createdAt: -1 });
+
+    const userIds = posts.map((post) => post.userId);
+
+    const users = await User.find({ _id: { $in: userIds } });
+
+    const postOfUser = new Map(users.map((user) => [user._id.toString(), { userId: user._id.toString(), username: user.username, avatar: user.avatar }]));
+
+    const postsOfUserInfo = posts.map(post => ({
+      _id: post._id.toString(),
+      userId: postOfUser.get(post.userId.toString()) || { id: 'Unknown', username: 'Unknown', avatar: 'default-avatar-url' },
+      desc: post.desc,
+      img: post.img,
+      likes: post.likes,
+      createdAt: post.createdAt,
+      updatedAt: post.updatedAt,
+    }));
+
+    if (postsOfUserInfo) {
+      return res.status(200).send({
+        status: 200,
+        message: "success",
+        postData: postsOfUserInfo,
+      });
+    } else {
+      return res.status(500).send({
+        status: 500,
+        message: "Internal Server Error",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+const getPostByIdController = asyncHandler(async (req, res) => {
+  const userId = req.query['userId'];
+  if (!userId) {
     return res.status(401).send({
       status: 401,
       message: "Authentication failed",
     });
   }
-  const post = await Post.find();
+
+  const post = await Post.find({ userId: userId }).sort({ createdAt: -1 });
   if (post) {
     return res.status(200).send({
       status: 200,
@@ -53,9 +92,11 @@ const GetPostController = asyncHandler(async (req, res) => {
       message: "Internal Server Error",
     });
   }
+
 });
 
 module.exports = {
   CreatePostController,
   GetPostController,
+  getPostByIdController
 };
--- src/routes/Posts.route.js
+++ src/routes/Posts.route.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const { CreatePostController, GetPostController } = require("../../src/controllers/posts");
+const { CreatePostController, GetPostController, getPostByIdController } = require("../../src/controllers/posts");
 const { verifyAccessToken } = require("../../helpers/jwtHelper");
 
 router.post("/create", verifyAccessToken, CreatePostController);
 router.get("/get-all", verifyAccessToken, GetPostController)
+router.get("/get-by-id", verifyAccessToken, getPostByIdController)
 
 module.exports = router;
